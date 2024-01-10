78589a7b0a82acef7f7e336a94bd9b517d464bcf
iamdeveloperdevesh <156159713+iamdeveloperdevesh@users.noreply.github.com>
2024-01-10 13:43:48 +0530

    Forgot Password Validation Done

--- application/controllers/forgot_password.php
+++ application/controllers/forgot_password.php
@@ -7,17 +7,26 @@ class forgot_password extends CI_Controller {
         // Load dashboard view
         $this->load->view('forgot_password');
     }
-    public function verify_password(){
-        $password = $this->input->post('password');
-        $confirm_password = $this->input->post('confirm_password');
 
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+    public function handleChangePassword(){
 
-        if($this->form_validation->run()== FALSE){
-            $this->load->view('forgot_password');
+        if($this->input->post()) {
+            $password = $this->input->post('password');
+            $confirm_password = $this->input->post('confirm_password');
+    
+            $this->load->library('form_validation');
+            $this->form_validation->set_error_delimiters('<div id="validationServerUsernameFeedback" class="invalid-feedback">', '</div>');
+            $this->form_validation->set_rules('password', 'Password', 'required');
+            $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+    
+            if($this->form_validation->run() == FALSE){
+                
+            }
+            else {
+                redirect('login');
+            }
         }
+        $this->load->view('forgot_password');
 
     }
 
--- application/views/forgot_password.php
+++ application/views/forgot_password.php
@@ -1,15 +1,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<form class="container" method="post" action="process_login">
+<form class="container" method="post" action="<?php echo base_url() . 'forgot_password/handleChangePassword'; ?>">
     <h2 class="m-5 d-flex justify-content-center">Forgot Password</h2>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter New Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" required>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword2" required>
+  <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+  <input type="password" class="form-control <?php echo form_error('confirm_password') !== "" ? 'is-invalid' : '' ?>" id="exampleInputPassword2" name="confirm_password" required>
+  <?php echo form_error('confirm_password'); ?>
   </div>
   <button type="submit" class="btn btn-primary">Change Password</button>
 </form>
