a598a3ebdaa1d88168dc313bb2243462bad67611
dalvisahil25 <155730834+dalvisahil25@users.noreply.github.com>
2024-01-10 12:32:52 +0530

    Forgot Password Changes

--- application/controllers/forgot_password.php
+++ application/controllers/forgot_password.php
@@ -7,5 +7,21 @@ class forgot_password extends CI_Controller {
         // Load dashboard view
         $this->load->view('forgot_password');
     }
+    public function verify_password(){
+        $password = $this->input->post('password');
+        $confirm_password = $this->input->post('confirm_password');
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+
+        if($this->form_validation->run()== FALSE){
+            $this->load->view('forgot_password');
+        }
+
+    }
+
+
 }
+
 ?>
